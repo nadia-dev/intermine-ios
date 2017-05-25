617132ee360c699dde8c7c7b19fa10658367deef
Nadia Yudina <joystate@me.com>
2017-05-25 12:45:46 +0200

    Add sliding menu

--- intermine-ios/BaseViewController.swift
+++ intermine-ios/BaseViewController.swift
@@ -8,16 +8,39 @@
 
 import UIKit
 
-class BaseViewController: UIViewController, UIViewControllerTransitioningDelegate {
+class BaseViewController: UIViewController {
+    
+    let interactor = Interactor()
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     func menuButtonPressed() {
-        print("tapped")
-        // TODO: implement sliding menu
+        // present VC modaly
+        if let menuVC = MenuViewController.menuViewController() {
+            menuVC.transitioningDelegate = self
+            menuVC.interactor = interactor
+            present(menuVC, animated: true, completion: nil)
+        }
     }
+}
 
 
+extension BaseViewController: UIViewControllerTransitioningDelegate {
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return PresentMenuAnimator()
+    }
+    
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return DismissMenuAnimator()
+    }
+    
+    func interactionControllerForDismissal(using animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+        return interactor.hasStarted ? interactor : nil
+    }
+    
+    func interactionControllerForPresentation(using animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+        return interactor.hasStarted ? interactor : nil
+    }
 }
